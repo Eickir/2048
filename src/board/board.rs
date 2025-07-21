9b425f78c3dbe3e79adf9e1468b91072fc337b0c
Richard Lavoura <richardlavoura@macbookpro.home>
2025-07-21 15:23:55 +0200

    feat: reorganize all modules into one board module

--- src/board/board.rs
+++ src/board/board.rs
@@ -1,6 +1,9 @@
 use rand::Rng;
+use strum::IntoEnumIterator;
 use rand::prelude::SliceRandom;
+use crate::utils::{zero_position, count_non_zero_value, trailing_zero_rows};
 use crate::board::constants::{ROWS, COLS};
+use crate::Move;
 
 
 #[derive(Debug)]
@@ -29,6 +32,7 @@ impl Board {
             is_game_over: false
         }
     }
+
 }
 
 // Méthodes
@@ -56,6 +60,112 @@ impl Board {
         }
     }
 
+    fn reverse_rows(&mut self) -> &mut Self {
+            for row in self.board_game.iter_mut() {
+                row.reverse();
+            }
+
+            self
+        }
+
+    fn transpose(&mut self) -> &mut Self {
+        let n = self.board_game.len();
+        for i in 0..n {
+            for j in i + 1..n {
+                let tmp = self.board_game[i][j];
+                self.board_game[i][j] = self.board_game[j][i];
+                self.board_game[j][i] = tmp;
+            }
+        }
+
+        self
+    }
+
+    fn change_board<'a>(&'a mut self, player_input: &Move, board_has_been_checked: bool) -> &'a mut Board {
+
+        if !board_has_been_checked {
+            match player_input {
+                Move::Left => self, 
+                Move::Right => self.reverse_rows(), 
+                Move::Up => self.transpose(), 
+                Move::Down => {
+                    self.transpose();
+                    self.reverse_rows();
+                    self
+                }
+            }
+        } else {
+            match player_input {
+                Move::Left => self, 
+                Move::Right => self.reverse_rows(), 
+                Move::Up => self.transpose(), 
+                Move::Down => {
+                    self.reverse_rows();
+                    self.transpose();
+                    self
+                }
+            }        
+        }
+
+    }
+
+    fn board_has_only_trailing_zeros(&self) -> bool {
+
+        let mut rows: Vec<u8> = Vec::new();
+        for row in &self.board_game {
+            let zero_position = zero_position(row);
+            let non_zero_occurences = count_non_zero_value(row);
+            if zero_position < non_zero_occurences {
+                rows.push(0);
+            } else {
+                rows.push(1);
+            }
+        }
+
+        trailing_zero_rows(&rows)
+
+    }
+
+    fn is_fusion_possible(&self) -> bool {
+            
+        for row in self.board_game.iter() {
+            let mut i = 0;
+            while i + 1 < row.len() {
+                if row[i] != 0 && row[i] == row[i + 1] {
+                    return true;
+                } else {
+                    i += 1;
+                }
+            }
+        }
+
+        false
+    }
+
+    fn is_move_possible(&self) -> bool {
+        
+        let trailing_zero = self.board_has_only_trailing_zeros();
+        let fusion_possible = self.is_fusion_possible();
+
+        if !trailing_zero || fusion_possible {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn is_game_over(&mut self) {
+        let any_possible = Move::iter().any(|direction| {
+            self.change_board(&direction, false);
+            let possible = self.is_move_possible();
+            self.change_board(&direction, true);
+            possible
+        });
+
+        self.is_game_over = !any_possible;
+
+    }
+
     fn zero_coordinates(&self) -> Vec<(usize, usize)> {
         let mut zero_coordinates: Vec<(usize, usize)> = Vec::new();
         for row in self.board_game.iter().enumerate() {
@@ -68,14 +178,14 @@ impl Board {
         zero_coordinates
     }
 
-    pub fn move_tiles(&mut self) -> &mut Self {
+    fn move_tiles(&mut self) -> &mut Self {
             for row in self.board_game.iter_mut() {
                 row.retain(|&x| x != 0);
             }
             self
         }
 
-    pub fn sum_tiles(&mut self) -> &mut Self {
+    fn sum_tiles(&mut self) -> &mut Self {
         for row in self.board_game.iter_mut() {
             let mut i = 0;
             while i + 1 < row.len() {
@@ -97,7 +207,7 @@ impl Board {
         self
     }
 
-    pub fn spawn_tiles(&mut self) -> &mut Self {
+    fn spawn_tiles(&mut self) -> &mut Self {
         let all_zero_coordinates = self.zero_coordinates();
         let mut rng = rand::rng();
         let new_tile_value = if rng.random_bool(0.9) { 2 } else { 4 };
@@ -107,5 +217,21 @@ impl Board {
 
         self
     }
+
+    pub fn make_move(&mut self, player_input: &Move) {
+
+            self.change_board(player_input, false);
+            if self.is_move_possible() {
+                self.move_tiles()
+                    .sum_tiles();
+                self.change_board(player_input, true);
+                self.spawn_tiles();
+            } else {
+                self.change_board(player_input, true);
+            }
+
+            self.is_game_over();
+            
+        }
 
 }
--- src/board/utils.rs
+++ src/board/utils.rs
@@ -1,94 +1,4 @@
-use crate::Board;
-use crate::Move;
-use strum::IntoEnumIterator;
-
-pub fn change_board<'a>(board: &'a mut Board, player_input: &Move, board_has_been_checked: bool) -> &'a mut Board {
-
-    if !board_has_been_checked {
-        match player_input {
-            Move::Left => board, 
-            Move::Right => reverse_rows(board), 
-            Move::Up => transpose(board), 
-            Move::Down => {
-                transpose(board);
-                reverse_rows(board);
-                board
-            }
-        }
-    } else {
-        match player_input {
-            Move::Left => board, 
-            Move::Right => reverse_rows(board), 
-            Move::Up => transpose(board), 
-            Move::Down => {
-                reverse_rows(board);
-                transpose(board);
-                board
-            }
-        }        
-    }
-
-}
-
-fn board_has_only_trailing_zeros(board: &Board) -> bool {
-
-    let mut rows: Vec<u8> = Vec::new();
-    for row in &board.board_game {
-        let zero_position = zero_position(row);
-        let non_zero_occurences = count_non_zero_value(row);
-        if zero_position < non_zero_occurences {
-            rows.push(0);
-        } else {
-            rows.push(1);
-        }
-    }
-
-    trailing_zero_rows(&rows)
-
-}
-
-fn is_fusion_possible(board: &Board) -> bool {
-        
-    for row in board.board_game.iter() {
-        let mut i = 0;
-        while i + 1 < row.len() {
-            if row[i] != 0 && row[i] == row[i + 1] {
-                return true;
-            } else {
-                i += 1;
-            }
-        }
-    }
-
-    false
-}
-
-pub fn is_move_possible(board: &Board) -> bool {
-    
-    let trailing_zero = board_has_only_trailing_zeros(&board);
-    let fusion_possible = is_fusion_possible(&board);
-
-    if !trailing_zero || fusion_possible {
-        true
-    } else {
-        false
-    }
-}
-
-
-pub fn is_game_over(board: &mut Board) {
-    let any_possible = Move::iter().any(|direction| {
-        change_board(board, &direction, false);
-        let possible = is_move_possible(board);
-        change_board(board, &direction, true);
-        possible
-    });
-
-    board.is_game_over = !any_possible;
-
-}
-
-fn count_non_zero_value(array: &Vec<u32>) -> u32 {
+pub fn count_non_zero_value(array: &Vec<u32>) -> u32 {
 
     let mut occurences: u32 = 0;
     for element in array {
@@ -101,38 +11,17 @@ fn count_non_zero_value(array: &Vec<u32>) -> u32 {
 
 } 
 
-fn zero_position(array: &Vec<u32>) -> u32 {
+pub fn zero_position(array: &Vec<u32>) -> u32 {
 
     array.iter().position(|&x|x==0).unwrap_or(4) as u32
 
 } 
 
-fn trailing_zero_rows(array: &Vec<u8>) -> bool {
+pub fn trailing_zero_rows(array: &Vec<u8>) -> bool {
 
     let result: u8 = array.into_iter().sum();
     if result < 4 {
         return false;
     }
     true
-}
-
-fn reverse_rows(board: &mut Board) -> &mut Board {
-        for row in board.board_game.iter_mut() {
-            row.reverse();
-        }
-
-        board
-    }
-
-fn transpose(board: &mut Board) -> &mut Board {
-    let n = board.board_game.len();
-    for i in 0..n {
-        for j in i + 1..n {
-            let tmp = board.board_game[i][j];
-            board.board_game[i][j] = board.board_game[j][i];
-            board.board_game[j][i] = tmp;
-        }
-    }
-
-    board
 }
